cadfdc32ea63d452388591e157bc059a7e656818
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-08-10 19:45:36 -0300

    feat: implements task handlers and create crud routes

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 
 	"github.com/alexandrejuniorc/simple-crud-go/config"
+	"github.com/alexandrejuniorc/simple-crud-go/handlers"
 	"github.com/alexandrejuniorc/simple-crud-go/models"
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -15,14 +17,27 @@ func main() {
 	// Chama a função setupDatabase
 	dbConnection := config.SetupDatabase()
 
+	// Chama a função Close para fechar a conexão com o banco de dados
+	defer dbConnection.Close()
+
 	// Executa a declaração SQL pra criar a tabela
 	_, err := dbConnection.Exec(models.CreateTableSQL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Chama a função Close para fechar a conexão com o banco de dados
-	defer dbConnection.Close()
+	router := mux.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	taskHandler := handlers.NewTaskHandler(dbConnection)
+
+	router.HandleFunc("/tasks", taskHandler.ReadTasks).Methods("GET")
+	router.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
+	// Path parameter
+	// PUT /tasks/123213
+	router.HandleFunc("/tasks/{id}", taskHandler.UpdateTask).Methods("PUT")
+	// Path parameter
+	// DELETE /tasks/123213
+	router.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
